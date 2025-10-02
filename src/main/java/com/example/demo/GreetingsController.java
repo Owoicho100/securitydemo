96c1d576e7ad8f485174ed549745713c51a765bb
Sunday John <owoichojohn80@gmail.com>
2025-10-02 13:51:42 +0100

    feature changes for PR

--- src/main/java/com/example/demo/GreetingsController.java
+++ src/main/java/com/example/demo/GreetingsController.java
@@ -8,7 +8,7 @@ public class GreetingsController {
 
     @GetMapping("/hello")
     public String sayHello(){
-        System.out.println("It's a new dad!");
+        System.out.println("Feature changes for PR!");
         return "Hello, World!";
     }
 }
